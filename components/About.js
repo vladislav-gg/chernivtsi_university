970b1fe329faabb568195ff8548ec3fdd4e3eaab
Vladislav <selies@mail.ru>
2021-12-11 03:37:23 +0000

    fix

--- components/About.js
+++ components/About.js
@@ -14,7 +14,7 @@ export default function About() {
                         <h3>Chernivtsi National University</h3>
                     </div>
                     <div className={aboutStyles.paragraph_wrapper}>
-                        <p>Here is the description of this project. I created a website for my hometown <u><Link href='http://www.chnu.edu.ua/index.php?page=ua'>University</Link></u>. The project is build with Next.js. For the first time I've used <em>map</em> function to loop through an array of objects which generated 5 separate components with different content (Majors component). I also want to add a registration form and connect it to a database for some unknown purpose. This is going to be my final project, after its completion I will start actively looking for a job as a front-end developer.</p>    
+                        <p>Here is the description of this project. I created a website for my hometown <u><Link href='http://www.chnu.edu.ua/index.php?page=ua'>University</Link></u>. The project is build with Next.js. For the first time I've used <em>map</em> function to loop through an array of objects which generated 5 separate components with different content. I also want to add a registration form and connect it to a database for some unknown purpose. This is going to be my final project, after its completion I will start actively looking for a job as a front-end developer.</p>    
                     </div>
                 </div>
             </div>
