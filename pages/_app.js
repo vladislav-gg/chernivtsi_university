c91b37c1adef3a0f8bff3a7328253d698cb56c89
Vladislav <selies@mail.ru>
2022-01-07 15:31:04 +0000

    one last attempt to fix this bug on mobile devices

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,19 @@ import '../styles/globals.css'
 import Navigation from '../components/Navigation'
 import Layout from '../components/Layout'
 import { AnimatePresence } from "framer-motion"
-
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
+    <>        
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
     <AnimatePresence>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </AnimatePresence>
+    </>
   )
 }
 
