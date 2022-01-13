503ea523fb9023f9732c847280011d88629f87fc
Vladislav <selies@mail.ru>
2022-01-13 12:49:05 +0000

    another small bug fix with next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,3 @@ module.exports = {
   reactStrictMode: true,
  
 }
-const production = process.env.NODE_ENV === "production";
-
-export const url = production
-  ? "https://www.chernivtsiuniversity.vercel.app"
-  : "https://localhost:1337";
