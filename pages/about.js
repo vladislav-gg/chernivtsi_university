4285d9ca0f0bb7179e2f950ae70ee5cfe65f3ad2
Vladislav <selies@mail.ru>
2022-02-05 23:48:22 +0000

    did some changes to about page and component

--- pages/about.js
+++ pages/about.js
@@ -8,9 +8,9 @@ export default function about() {
         <div className={aboutStyles.about_page_wrapper}>
             <div className={aboutStyles.about_page_container}>
             
-                <div className={aboutStyles.paragraph_wrapper}>
-                    <p>Here is the description of this project. I created a website for my hometown <u><Link href='http://www.chnu.edu.ua/index.php?page=ua'>University</Link></u>. The project is build with Next.js. For the first time Ive used <em>map</em> function to loop through an array of objects which generated 5 separate components with different content (Majors component). I also want to add a registration form and connect it to a database for some unknown purpose. This is going to be my final project, after its completion I will start actively looking for a job as a front-end developer.</p>    
-                </div>
+            <div className={aboutStyles.paragraph_wrapper}>
+                        <p>I created a website for my hometown <u><Link href='http://www.chnu.edu.ua/index.php?page=ua'>University</Link></u>. I had this idea for a project about 5 years ago, when I started getting into web design and typography. At that time I was drawing websites in Photoshop and they were pretty mediocre, but I still liked the process and learned a lot. The project is built with Nextjs. For the first time I've fetched data from a custom database (data.js) and used <em>map</em> function to loop through an array of objects which generated 5 separate components with different content (Majors component). I have also used Strapi as headless CMS and fetched data from it's API at <u><Link href='/news'>news</Link></u> page.</p>    
+                </div>   
              </div>        
     
         </div>
