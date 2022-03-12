bb88f66944401e376fe0886bfd1aaa61ecbf8e62
Vladislav <selies@mail.ru>
2022-03-12 22:58:19 +0000

    news page is responsive

--- components/News_page_component.js
+++ components/News_page_component.js
@@ -1,43 +1,41 @@
-import React from 'react'
-import newsStyles from '../styles/News.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import newsStyles from "../styles/News.module.css";
+import Link from "next/link";
+import Image from "next/image";
 import { getStrapiMedia } from "../lib/media";
 import { fetchAPI } from "../lib/api";
-import { url } from "../next.config"
-
+import { url } from "../next.config";
 
 export default function News_page_component(props) {
-    console.log(props.src)
-    return (
-        <div>
-            <div className={newsStyles.news_main}>
-                <Link href='/'>    
-                    <a className={newsStyles.news_links}>
-                        <div className={newsStyles.news_item}>
-                            <div className={newsStyles.image_container}>
-                                <Image 
-                                    src={props.src}
-                                    id={props.id}
-                                    alt='title-image' 
-                                    layout='fill'
-                                    objectFit="cover" 
-                                    className={newsStyles.news_image}
-                                />
-                            </div>
-                            <div className={newsStyles.description_container}>
-                                <div className={newsStyles.news_tags}>
-                                    <p className={newsStyles.news_date}>13 December, 03:55</p> 
-                                    <p className={newsStyles.news_tag}>Basic</p>
-                                </div>
-                                <div className={newsStyles.h3_styles}>{props.title}</div>
-                                <p className={newsStyles.p_styles}> {props.description}
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                </Link>
-            </div>
-        </div>
-    )
+	console.log(props.src);
+	return (
+		<div>
+			<div className={newsStyles.news_main}>
+				<Link href="/">
+					<a className={newsStyles.news_links}>
+						<div className={newsStyles.news_item}>
+							<div className={newsStyles.image_container}>
+								<Image
+									src={props.src}
+									id={props.id}
+									alt="title-image"
+									layout="fill"
+									objectFit="cover"
+									className={newsStyles.news_image}
+								/>
+							</div>
+							<div className={newsStyles.description_container}>
+								<div className={newsStyles.news_tags}>
+									<p className={newsStyles.news_date}>13 December, 03:55</p>
+									<p className={newsStyles.news_tag}>Basic</p>
+								</div>
+								<div className={newsStyles.h3_styles}>{props.title}</div>
+								<p className={newsStyles.p_styles}> {props.description}</p>
+							</div>
+						</div>
+					</a>
+				</Link>
+			</div>
+		</div>
+	);
 }
--- pages/news.js
+++ pages/news.js
@@ -1,55 +1,46 @@
-import React from 'react'
-import newsStyles from '../styles/News.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
-import News_page_component from '../components/News_page_component'
+import React from "react";
+import newsStyles from "../styles/News.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import News_page_component from "../components/News_page_component";
 import { getStrapiMedia } from "../lib/media";
 import { fetchAPI } from "../lib/api";
-import { url } from "../next.config"
-import { useState } from 'react'
-
-
+import { url } from "../next.config";
+import { useState } from "react";
 
 export default function News_section() {
+	const URL = "http://localhost:1337";
+	const herokuURL = "https://chernivtsiuniversity.herokuapp.com";
+	const [topics, setTopics] = useState(null);
+	const getTopics = async () => {
+		const res = await fetch(herokuURL + "/api/topics?populate=%2A");
+		const json = await res.json();
+		setTopics(json);
+	};
 
+	if (!topics) {
+		getTopics();
+	}
 
-  const URL = 'http://localhost:1337'
-  const herokuURL = 'https://chernivtsiuniversity.herokuapp.com'
-  const [topics, setTopics] = useState(null);
-  const getTopics = async() => {
-    const res = await fetch(herokuURL + "/api/topics?populate=%2A");
-    const json = await res.json();
-    setTopics(json);
-    
-  }
-
-  if(!topics){
-    getTopics();
-  }
-
-    return (
-        <div className={newsStyles.news_wrapper}>
-            <div className={newsStyles.news_header}>
-                <h1>News and events</h1>
-                <div>
-                {topics && topics.data.map ((topic) => {
-
-                  return (
-                    
-                    <News_page_component
-                      key={topic.id}
-                      id={topic.id}
-                      title={topic.attributes.title}
-                      description={topic.attributes.description}
-                      src={topic.attributes.image.data.attributes.url}
-                    >
-                    </News_page_component>
-                    
-                  )
-                })}
-                </div>
-            </div>
-        </div>
-    )
-};
-
+	return (
+		<div className={newsStyles.news_wrapper}>
+			<div className={newsStyles.news_header}>
+				<h1>News and events</h1>
+				<div>
+					{topics &&
+						topics.data.map((topic) => {
+							return (
+								<News_page_component
+									key={topic.id}
+									id={topic.id}
+									title={topic.attributes.title}
+									description={topic.attributes.description}
+									src={topic.attributes.image.data.attributes.url}
+								></News_page_component>
+							);
+						})}
+				</div>
+			</div>
+		</div>
+	);
+}
