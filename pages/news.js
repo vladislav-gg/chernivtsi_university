99d05ab8d7eaedaa0f6b5a45d366e1a2f8318658
Vladislav <selies@mail.ru>
2022-03-13 02:01:36 +0000

    added SWR and fetched Strapi API on /news page

--- pages/news.js
+++ pages/news.js
@@ -7,28 +7,50 @@ import { getStrapiMedia } from "../lib/media";
 import { fetchAPI } from "../lib/api";
 import { url } from "../next.config";
 import { useState } from "react";
+import useSWR from "swr";
+import loaderStyles from "../styles/Loderr.module.css";
+import { motion } from "framer-motion";
 
 export default function News_section() {
+	const fetcher = (...args) => fetch(...args).then((res) => res.json());
 	const URL = "http://localhost:1337";
 	const herokuURL = "https://chernivtsiuniversity.herokuapp.com";
-	const [topics, setTopics] = useState(null);
-	const getTopics = async () => {
-		const res = await fetch(herokuURL + "/api/topics?populate=%2A");
-		const json = await res.json();
-		setTopics(json);
-	};
+	// const [topics, setTopics] = useState(null);
+	// const getTopics = async () => {
+	// 	const res = await fetch(herokuURL + "/api/topics?populate=%2A");
+	// 	const json = await res.json();
+	// 	setTopics(json);
+	// };
 
-	if (!topics) {
-		getTopics();
+	// if (!topics) {
+	// 	getTopics();
+	// }
+	const { data, error } = useSWR(
+		herokuURL + "/api/topics?populate=%2A",
+		fetcher
+	);
+
+	if (error) {
+		return (
+			<div className={loaderStyles.news_error}>
+				<h1>Failed to load.</h1>
+			</div>
+		);
 	}
+	if (!data)
+		return (
+			<div className={loaderStyles.news_loading}>
+				<h1>Loading...</h1>
+			</div>
+		);
 
 	return (
 		<div className={newsStyles.news_wrapper}>
 			<div className={newsStyles.news_header}>
 				<h1>News and events</h1>
 				<div>
-					{topics &&
-						topics.data.map((topic) => {
+					{data &&
+						data.data.map((topic) => {
 							return (
 								<News_page_component
 									key={topic.id}
