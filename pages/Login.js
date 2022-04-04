c54d7aa67b3a1fff2f22a819fad30610ac3753e2
Vladislav <selies@mail.ru>
2022-04-04 03:46:54 +0100

    login page functionality is working and styled

--- pages/Login.js
+++ pages/Login.js
@@ -1,13 +1,58 @@
 import loginStyles from "../styles/Login.module.css";
-import { auth, signInWithEmailAndPassword, signInWithGoogle } from "./firebase";
+import {
+	auth,
+	signInWithEmailAndPassword,
+	signInWithGoogle,
+} from "../lib/firebase.js";
 import { useAuthState } from "react-firebase-hooks/auth";
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
-export default function Login() {
+export default function login() {
+	const [user, loading, error] = useAuthState(auth);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [user, loading, error] = useAuthState(auth);
-	const navigate = useNavigate();
 
-	return <div>Login</div>;
+	return (
+		<div className={loginStyles.login_container}>
+			<div className={loginStyles.login_wrapper}>
+				<input
+					type="text"
+					className={loginStyles.login_inputs}
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					placeholder="E-mail Address"
+				/>
+				<input
+					type="password"
+					className={loginStyles.login_inputs}
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder="Password"
+				/>
+				<div className={loginStyles.buttons_wrapper}>
+					<button
+						className={loginStyles.login_button}
+						onClick={() => signInWithEmailAndPassword(email, password)}
+					>
+						Login
+					</button>
+					<button
+						className={loginStyles.login_button}
+						onClick={signInWithGoogle}
+					>
+						Login with Google
+					</button>
+				</div>
+				<div className={loginStyles.lower_section}>
+					<div>
+						<Link href="/reset">Forgot Password</Link>
+					</div>
+					<div>
+						Don't have an account? <Link href="/register">Register</Link> now.
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
