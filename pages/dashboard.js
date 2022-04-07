bdadafdfcdd36a04648b4a0d827bf4533d8f3bab
Vladislav <selies@mail.ru>
2022-04-07 02:58:15 +0100

    added dashboard component

---
+++ pages/dashboard.js
@@ -0,0 +1,35 @@
+import dashboardStyles from "../styles/Dashboard.module.css";
+import { auth, db, logout } from "../lib/firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { query, collection, getDocs, where } from "firebase/firestore";
+
+export default function dashboard() {
+	const [user, loading, error] = useAuthState(auth);
+	const [name, setName] = useState("");
+	const fetchUserName = async () => {
+		try {
+			const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+			const doc = await getDocs(q);
+			const data = doc.docs[0].data();
+			setName(data.name);
+		} catch (err) {
+			console.error(err);
+			alert("An error occured while fetching user data");
+		}
+	};
+
+	return (
+		<div className={dashboardStyles.dash_wrapper}>
+			<div className={dashboardStyles.dash_container}>
+				Logged in as
+				<div>{name}</div>
+				<div>{user?.email}</div>
+				<button className={dashboardStyles.button} onClick={logout}>
+					Logout
+				</button>
+			</div>
+		</div>
+	);
+}
