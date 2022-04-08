34c1b02715ca95e9efb6592dfd5e8543e3f2310c
Vladislav <selies@mail.ru>
2022-04-08 21:35:50 +0100

    added auth in navigation

--- components/Majors.js
+++ components/Majors.js
@@ -1,37 +1,32 @@
-import React from 'react'
-import majorStyles from '../styles/Majors.module.css'
-import Link from 'next/link'
-import { motion, AnimatePresence } from "framer-motion"
-import Image from 'next/dist/client/image'
+import React from "react";
+import majorStyles from "../styles/Majors.module.css";
+import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/dist/client/image";
 import { useState } from "react";
-import Collapsible from './Collapsible'
-import { data } from './data'
-
+import Collapsible from "./Collapsible";
+import { data } from "./data";
 
 export default function Majors() {
-    return (
-        <div className={majorStyles.major_wrapper}>
-            <div className={majorStyles.major_container}>
-                <div className={majorStyles.major_heading}>
-                    <h1>Majors</h1>
-                </div>
-                {
-                data.map(major => {
-                    console.log(major.href)
-
-                    return (
-                    <Collapsible
-                        key={major.id.toString()}
-                        id={major.id}
-                        description={major.description}
-                        image={major.image}
-                        name={major.name}
-                        href={major.href}
-                    />
-                )})}
-            </div>
-        </div>
-    )
+	return (
+		<div className={majorStyles.major_wrapper}>
+			<div className={majorStyles.major_container}>
+				<div className={majorStyles.major_heading}>
+					<h1>Majors</h1>
+				</div>
+				{data.map((major) => {
+					return (
+						<Collapsible
+							key={major.id.toString()}
+							id={major.id}
+							description={major.description}
+							image={major.image}
+							name={major.name}
+							href={major.href}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
-
-
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,85 +1,125 @@
-import React from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import navStyles from '../styles/Navigation.module.css'
-import Link from 'next/link'
-import Burger from './Burger'
-import { useState } from 'react'
-import burgerStyle from '../styles/Burger.module.css'
-import { motion } from "framer-motion"
-
-
-
+import Head from "next/head";
+import Image from "next/image";
+import navStyles from "../styles/Navigation.module.css";
+import Link from "next/link";
+import Burger from "./Burger";
+import { useState } from "react";
+import burgerStyle from "../styles/Burger.module.css";
+import { motion } from "framer-motion";
+import { auth, db, logout } from "../lib/firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import React, { useEffect } from "react";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import { useRouter } from "next/router";
+import dashboardStyles from "../styles/Dashboard.module.css";
 
 export default function Navigation() {
-    const variants = {
-        hidden: { opacity: 0, x: 0, y: -200 },
-        enter: { opacity: 1, x: 0, y: 0 },
-        exit: { opacity: 0, x: 0, y: -100 },
-    }
-    
-    const [ burgerOpen, setBurgerOpen ] = useState(false)
-    const  toggleBurger = () =>{
-        setBurgerOpen(!burgerOpen)
-    }
+	const router = useRouter();
+	const [user, loading, error] = useAuthState(auth);
+	const [name, setName] = useState("");
+	const fetchUserName = async () => {
+		try {
+			const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+			const doc = await getDocs(q);
+			const data = doc.docs[0].data();
+			setName(data.name);
+		} catch (err) {
+			console.error(err);
+			alert("An error occured while fetching user data");
+		}
+	};
+	useEffect(() => {
+		if (loading) return;
+		if (!user) return router.push("/");
 
+		fetchUserName();
+	}, [user, loading]);
 
+	const variants = {
+		hidden: { opacity: 0, x: 0, y: -200 },
+		enter: { opacity: 1, x: 0, y: 0 },
+		exit: { opacity: 0, x: 0, y: -100 },
+	};
 
-    return (
-        <motion.div 
-            variants={variants}
-            initial='hidden'
-            animate='enter'
-            exit='exit'
-            transition={{
-                type: 'linear',
-                duration: 1.4
-                }}            
+	const [burgerOpen, setBurgerOpen] = useState(false);
+	const toggleBurger = () => {
+		setBurgerOpen(!burgerOpen);
+	};
 
-        className={navStyles.nav_wrapper}>
-            <div className={navStyles.nav_links_container}>
-                    <div className={navStyles.nav_links}>
-                        <Link href='/admissions'>
-                            <a className={navStyles.nav_links_1}>Admissions</a>
-                        </Link>
-                        <Link href='/news'>
-                            <a className={navStyles.nav_links_1}>News & Events</a>
-                        </Link>
-                        <Link href='/about'>
-                            <a className={navStyles.nav_links_1}>About</a>
-                        </Link>
-                    </div>
-                </div>
-                <div className={navStyles.burger_container}>
-                    <div className={navStyles.burger_toggle}>
-                        <div className='burger' onClick={toggleBurger}>
-                            <Burger/>
-                        </div>
-                        <div className={navStyles.nav_links_container_mobile}>
-                                <div className={navStyles.nav_links_mobile} style={{display:burgerOpen ? 'flex' : 'none'}}>
-                                    <Link href='/admissions'>
-                                        <a className={navStyles.nav_links_1_mobile}>Admissions</a>
-                                    </Link>
-                                    <Link href='/news'>
-                                        <a className={navStyles.nav_links_1_mobile}>News & Events</a>
-                                    </Link>
-                                    <Link href='/about'>
-                                        <a className={navStyles.nav_links_1_mobile}>About</a>
-                                    </Link>
-                                </div>
-                            </div>
-                    <div>
-                    </div>
-                </div>
-                <Link href='/'>
-                    <a>
-                        <div className={navStyles.logo}>
-                            <div id={navStyles.first_word}>Chernivtsi</div>
-                            <div id={navStyles.second_word}>University</div> 
-                        </div>
-                    </a>
-                </Link>
-            </div>
-        </motion.div>
-    )
+	return (
+		<motion.div
+			variants={variants}
+			initial="hidden"
+			animate="enter"
+			exit="exit"
+			transition={{
+				type: "linear",
+				duration: 1.4,
+			}}
+			className={navStyles.nav_wrapper}
+		>
+			<div className={navStyles.nav_links_container}>
+				<div className={navStyles.nav_links}>
+					<Link href="/admissions">
+						<a className={navStyles.nav_links_1}>Admissions</a>
+					</Link>
+					<Link href="/news">
+						<a className={navStyles.nav_links_1}>News & Events</a>
+					</Link>
+					<Link href="/about">
+						<a className={navStyles.nav_links_1}>About</a>
+					</Link>
+					{user && <div className={navStyles.nav_links_1}>{name}.</div>}
+
+					{!user ? (
+						<>
+							<Link href="/Login">
+								<a className={navStyles.nav_links_1}>Login</a>
+							</Link>
+
+							<Link href="/register">
+								<a className={navStyles.nav_links_1}>Register</a>
+							</Link>
+						</>
+					) : (
+						<button className={dashboardStyles.button} onClick={logout}>
+							Sign out
+						</button>
+					)}
+				</div>
+			</div>
+			<div className={navStyles.burger_container}>
+				<div className={navStyles.burger_toggle}>
+					<div className="burger" onClick={toggleBurger}>
+						<Burger />
+					</div>
+					<div className={navStyles.nav_links_container_mobile}>
+						<div
+							className={navStyles.nav_links_mobile}
+							style={{ display: burgerOpen ? "flex" : "none" }}
+						>
+							<Link href="/admissions">
+								<a className={navStyles.nav_links_1_mobile}>Admissions</a>
+							</Link>
+							<Link href="/news">
+								<a className={navStyles.nav_links_1_mobile}>News & Events</a>
+							</Link>
+							<Link href="/about">
+								<a className={navStyles.nav_links_1_mobile}>About</a>
+							</Link>
+						</div>
+					</div>
+					<div></div>
+				</div>
+				<Link href="/">
+					<a>
+						<div className={navStyles.logo}>
+							<div id={navStyles.first_word}>Chernivtsi</div>
+							<div id={navStyles.second_word}>University</div>
+						</div>
+					</a>
+				</Link>
+			</div>
+		</motion.div>
+	);
 }
