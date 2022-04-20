4054cc934537ac5d28a8e3d376b5d7c92ae4186a
Vladislav <selies@mail.ru>
2022-04-20 02:26:29 +0100

    news fix£

--- components/News_page_component.js
+++ components/News_page_component.js
@@ -7,7 +7,6 @@ import { fetchAPI } from "../lib/api";
 import { url } from "../next.config";
 
 export default function News_page_component(props) {
-	console.log(props.src);
 	return (
 		<div>
 			<div className={newsStyles.news_main}>
