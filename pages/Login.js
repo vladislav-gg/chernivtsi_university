9c3d1ddce32a81098b76229be48124b89e968b2c
Vladislav <selies@mail.ru>
2022-05-13 17:28:33 +0100

    added aria-labelledby attribute to the form

--- pages/Login.js
+++ pages/Login.js
@@ -23,6 +23,7 @@ export default function Login() {
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
 					placeholder="E-mail Address"
+					aria-labelledby="Email address"
 				/>
 				<input
 					type="password"
@@ -30,6 +31,7 @@ export default function Login() {
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 					placeholder="Password"
+					aria-labelledby="Password"
 				/>
 				<div className={loginStyles.buttons_wrapper}>
 					<button
