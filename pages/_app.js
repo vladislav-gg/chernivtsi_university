f0487b767c29229a95f85411f3b218dd4d84d106
Vladislav <selies@mail.ru>
2022-05-21 23:49:48 +0100

    removed meta tag that shouldn't have been there

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,6 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Head>
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-			</Head>
 			<AnimatePresence>
 				<Layout>
 					<Component {...pageProps} />
